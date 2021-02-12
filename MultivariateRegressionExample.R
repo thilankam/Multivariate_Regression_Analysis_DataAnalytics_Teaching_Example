3f419805a0ca396536a3c349a6c98271d38012c0
Thilanka Munasinghe <thilankamunasinghe@Thilankas-MacBook-Pro.local>
2021-02-12 15:54:38 -0500

    Added the Part 2 of the teaching example which include generating the plots and abline

--- MultivariateRegressionExample.R
+++ MultivariateRegressionExample.R
@@ -2,7 +2,10 @@
 multivariate <-read.csv("~/Downloads/multivariate.csv")
 head(multivariate)
 attach(multivariate)
-help(lm)
+
+ # Part 1 --
+help(lm) # Read the documentation for the lm() function.
+
 mm <-lm(Homeowners~Immigrant)
 mm # mm here is a R object. 
 summary(mm)$coef # The output above shows the estimate of the regression beta coefficients (column Estimate) and 
@@ -12,4 +15,27 @@ summary(mm)$coef # The output above shows the estimate of the regression beta co
 # Homeowners = 107494.898 + (-6656.839)*Immigrant 
 # We can rewrite it as: 
 # Homeowners = 107494.898 - 6656.839*Immigrant.
+
+ # Part 2 --
+
+plot(Homeowners~Immigrant) # Plot Homeowners Vs Immigrants
+# read the documentation for abline()
+help(abline)
+abline(mm)
+abline(mm,col=2,lwd=3)
+# Using this formula, for each new value in Immigrant, you can predict the value for Homeowners.
+# As an example:
+# For Immigrant value = 0, we will get: Homeowners = 107494.898 - 6656.839*0 = 107494.898
+# for Immigrant value = 20, we will get: Homeowners = 107494.898 - 6656.839*20 = -25641.88
+# Predictions can be easily made using the R function predict().
+# In the following example, we predict Homeowners for two Immigrant values: 0 and 20.
+# you can pass the 0 and 20 values as a concatenated list for Immigrants as follows:
+newImmigrantdata <- data.frame(Immigrant = c(0,  20))
+mm %>% predict(newImmigrantdata)
+
+abline(mm)
+abline(mm,col=3,lwd=3) # line color = green, line width = 3
+attributes(mm)
+mm$coefficients
+
 
